2cb1b01dcc37f02fc24023450a538efff1df9201
braineo <phantom2501@gmail.com>
2015-07-16 09:43:16 +0900

    getDay, Sunday is 0 not 7

--- cardUI.js
+++ cardUI.js
@@ -1,7 +1,7 @@
 /* cardUI
-* generate day and weekday every day
-* markout who did not punch in previous day
-*/
+ * generate day and weekday every day
+ * markout who did not punch in previous day
+ */
 
 // Day of year
 function dayOfYear(date) {
@@ -25,22 +25,29 @@ function dayRoutine() {
 function generateDayHead(sheet, row) {
     var today = new Date();
     sheet.getRange(row, 1).setValue(today);
-    var weekDay = { 1: '月', 2: '火', 3: '水', 4: '木', 5: '金', 6: '土', 7: '日' };
+    var weekDay = {
+        1: '月',
+        2: '火',
+        3: '水',
+        4: '木',
+        5: '金',
+        6: '土',
+        0: '日'
+    };
     sheet.getRange(row, 2).setValue(weekDay[today.getDay()]);
 }
 
 function markMiss(sheet, row) {
-  var header = sheet.getRange("1:1").getValues();
-  Logger.log(header);
-    for(var i=3; i<=header[0].length; i++){
-        if(sheet.getRange(row, i).isBlank()){
+    var header = sheet.getRange("1:1").getValues();
+    for (var i = 3; i <= header[0].length; i++) {
+        if (sheet.getRange(row, i).isBlank()) {
             sheet.getRange(row, i).setBackground("red");
         }
     }
 }
 
-function onOpen(e){
+function onOpen(e) {
     var row = dayOfYear(new Date());
     var punchCardSheet = SpreadsheetApp.openById('1d3jLeX_FcNEEq_bQvcK7LRKQnq01-8hKHH5JSR_HH5k').getSheetByName("Punch");
     SpreadsheetApp.setActiveRange(punchCardSheet.getRange(row + 10, 1)); //Move activate cell to recent date
-} 
+}
